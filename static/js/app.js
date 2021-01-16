f4872f1be7db9eb11a1ef21bfa7b290320e4477e
Loic Tiemani <loictiems@github.com>
2021-01-16 10:25:52 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -13,7 +13,7 @@ var tableData = data;
 data.forEach(function(tableBuilder) {
     console.log(tableBuilder);
     // Start By Clearing Existing Data
-     tbody.html("");
+     //tbody.html("");
     //Using d3 to append one table row `tr` for each data object
     var row =tbody.append("tr");
     
@@ -38,24 +38,45 @@ function handleClick(){
     // Select HTML Input Element & Get the Value Property of that Input Element
 
     var date = d3.select("#datetime").property("value");
-    var filterData = tableData;
+    //var filterData = tableData;
 
     // Check if a Date was Entered & Filter Data Using that Date;
     if (date) {
 
         // Apply Filter to the Table Data to Only Keep Rows Where datetime Value Matches the Filter Value
-        filterData = filterData.filter((row) => row.datetime === date);
+        filterData = tableData.filter((row) => row.datetime === date);
+        console.log(filterData)
     };
+    //clear table
+    tbody.html("")
 
     // Build Table with Filtered Data
-    tableBuilder(filterData);
-    };
-};
+    //tableBuilder(filterData);
+    filterData.forEach(function(tableBuilder) {
+        console.log(tableBuilder);
+        // Start By Clearing Existing Data
+         //tbody.html("");
+        //Using d3 to append one table row `tr` for each data object
+        var row =tbody.append("tr");
+        
+        Object.entries(tableBuilder).forEach(function([key, value]) {
+            console.log(key, value);
+    
+        // Use d3 to append 1 cell per report value (date/time, city, state, country, shape, and comment )
+        // Append a cell to the row for each value in the report object
+        var cell = row.append("td");
+        cell.text(value);
+    
+        });     
+    });
 
+};
 // `on` Function to attach an Event to the Handler Function
 d3.selectAll("#filter-btn").on("click",handleClick);
 
 // Build Table data.js
+//tableBuilder(tableData);
 
-tableBuilder(tableData);
+
+
 
